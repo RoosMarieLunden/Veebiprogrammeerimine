37e5968cae851a96bb2db43a0633d96a23bc1783
Roos-Marie Lunden <rooslun@users.noreply.github.com>
2019-09-10 11:09:10 +0300

    esimene üles laadimine

---
+++ 02 tund/testpage.php
@@ -0,0 +1,21 @@
+<?php
+  $userName = "Roos L Marie";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+  Veebm Programmeerib</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavad sisu.</p>
+</body>
+</html>
