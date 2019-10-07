12b38e99e981b77f508b582350e3a469becca9bf
Roos-Marie Lunden <roosml@tlu.ee>
2019-10-07 20:40:04 +0300

    viies tund
    viies tund

---
+++ 05 tund/myindex.php
@@ -0,0 +1,19 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>RoosML programmeerib veebi</title>
+
+</head>
+<body>
+  <h1>Roosi L, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</p>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+</body>
+</html>
